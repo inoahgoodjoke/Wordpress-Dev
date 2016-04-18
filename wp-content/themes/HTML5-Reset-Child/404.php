495b420edfc790d42919e85af075fab7a61e3815
inoahgoodjoke <noahjmead@gmail.com>
2016-04-18 13:37:56 -0700

    Updated 404

--- wp-content/themes/HTML5-Reset-Child/404.php
+++ wp-content/themes/HTML5-Reset-Child/404.php
@@ -5,9 +5,12 @@
  * @since HTML5 Reset 2.0
  */
  get_header(); ?>
-
-	<h2><?php _e('You done goofed','html5reset'); ?></h2>
-
-<?php get_sidebar(); ?>
-
+ <div class="row">
+	<section class="col-md-8">
+		<h2><?php _e('You done goofed','html5reset'); ?></h2>
+	</section>
+	<aside class="col-md-4">
+		<?php get_sidebar(); ?>
+	</aside>
+</div>
 <?php get_footer(); ?>
