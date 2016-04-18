27ee87c970537ffe2ea251b45def567fd2ada744
inoahgoodjoke <noahjmead@gmail.com>
2016-04-18 13:30:57 -0700

    Adding index.php and 2 column layou

--- wp-content/themes/HTML5-Reset-Child/404.php
+++ wp-content/themes/HTML5-Reset-Child/404.php
@@ -6,7 +6,7 @@
  */
  get_header(); ?>
 
-	<h2><?php _e('You done goofed','Good Job'); ?></h2>
+	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
 
 <?php get_sidebar(); ?>
 
--- wp-content/themes/HTML5-Reset-Child/functions.php
+++ wp-content/themes/HTML5-Reset-Child/functions.php
@@ -1,32 +1,21 @@
 <?php
 function theme_enqueue_styles() {
-
-	// Load Parent and Child Theme Files
-
     $parent_style = 'parent-style';
 
+	// Load Child Theme
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
-	
-	// Load Bootstrap Files
-	
+	// Load Bootstrap CSS Files
 	wp_enqueue_style( 'bootstrap-min',
 		get_stylesheet_directory_uri() . '/css/bootstrap.min.css'
 	);
 	wp_enqueue_style( 'bootstrap-theme-min',
 		get_stylesheet_directory_uri() . '/css/bootstrap-theme.min.css'
 	);
-	wp_enqueue_script( 'bootstrap-min',
-		get_stylesheet_directory_uri() . '/js/bootstap.min.js',
-		true
-	);
+	wp_enqueue_script( 'bootstrap.min', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', '3.0.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 ?>
-
-body {
-	font-family: Papyrus, fantasy;
-}
