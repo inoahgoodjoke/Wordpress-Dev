d5545ecbcc2ca29a520b9b87088385d83cbf8621
inoahgoodjoke <noahjmead@gmail.com>
2016-04-20 12:45:30 -0700

    Fixed functions

--- wp-content/themes/HTML5-Reset-Child/functions.php
+++ wp-content/themes/HTML5-Reset-Child/functions.php
@@ -1,21 +1,32 @@
 <?php
 function theme_enqueue_styles() {
+
+	// Load Parent and Child Theme Files
+
     $parent_style = 'parent-style';
 
-	// Load Child Theme
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style )
     );
-	// Load Bootstrap CSS Files
+	
+	// Load Bootstrap Files
+	
 	wp_enqueue_style( 'bootstrap-min',
 		get_stylesheet_directory_uri() . '/css/bootstrap.min.css'
 	);
 	wp_enqueue_style( 'bootstrap-theme-min',
 		get_stylesheet_directory_uri() . '/css/bootstrap-theme.min.css'
 	);
-	wp_enqueue_script( 'bootstrap.min', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', '3.0.0', true);
+	wp_enqueue_script( 'bootstrap-min',
+		get_stylesheet_directory_uri() . '/js/bootstap.min.js',
+		true
+	);
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 ?>
+
+body {
+	font-family: Papyrus, fantasy;
+}
